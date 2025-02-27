d91bf570748535052b86eda2ea6944b0f47600be
thomas <thomanit@ntnu.no>
2025-02-27 14:48:16 +0100

    Update Readme.md for new method, added needed files, renamed files

--- AI/firecrawl_app.py
+++ AI/firecrawl_app.py
@@ -2,20 +2,20 @@
     Sheet for initializing the Firecrawl App.
 """
 from firecrawl import FirecrawlApp
-from pydantic import BaseModel, Field
-from typing import Any, Optional, List
-
+from pydantic import BaseModel
 from dotenv import load_dotenv
 import os 
-import pprint
 
 # Load variables from .env into environment
 load_dotenv()
+
+#Set API key
 api_key = os.getenv("FIRECRAWL_API_KEY")
 
 # Initialize the Firecrawl app
 app = FirecrawlApp(api_key=api_key)
 
+# Model/Schema for the extracted data
 class NestedModel1(BaseModel):
     kunnskaper: list[str]
     ferdigheter: list[str]
@@ -23,13 +23,4 @@ class NestedModel1(BaseModel):
 
 class ExtractSchema(BaseModel):
     læringsutbyttebeskrivelser: NestedModel1
-
-# data = app.extract([
-#   "https://uio.no/studier/program/informatikk-ledelse/hva-lerer-du/"
-# ], {
-#     'prompt': 'Extract all descriptions under "kunnskaper", "ferdigheter", and "generell kompetanse" from the "læringsutbyttebeskrivelser" ("Hva lærer du") section. Make sure to retrieve the descriptions listed in bold font as well. ',
-#     'schema': ExtractSchema.model_json_schema(),
-# })
-
-# pprint.pprint(data)
 
