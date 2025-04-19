a216c46571b2c0c492286ea575ac0111690dc326
Karan Singh Sandhu <karan.s.01@hotmail.com>
2025-04-19 10:47:26 +0200

    added mandatory subjects-not done

---
+++ mandatory_subjects/firecrawl_app.py
@@ -0,0 +1,27 @@
+"""
+    Sheet for initializing the Firecrawl App.
+"""
+from firecrawl import FirecrawlApp
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os 
+
+# Load variables from .env into environment
+load_dotenv()
+
+#Set API key
+api_key = os.getenv("FIRECRAWL_API_KEY")
+
+# Initialize the Firecrawl app
+app = FirecrawlApp(api_key=api_key)
+
+# Model/Schema for the extracted data
+class NestedModel(BaseModel):
+    kunnskaper: list[str]
+    ferdigheter: list[str]
+    generell_kompetanse: list[str]
+    obligatorise_emner: list[str]
+
+class ExtractSchema(BaseModel):
+    læringsutbyttebeskrivelser: NestedModel
+
